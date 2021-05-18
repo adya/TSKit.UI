d65e4d7fa089798dcdae1198c6dfe8f598b919ae
Arkadii Hlushchevskyi <adya.glu@gmail.com>
2021-05-18 11:52:57 +0300

    Added UITraitCollection extension to determine current layout.

---
+++ Sources/TSKit.UI/UITraitCollection+Layout.swift
@@ -0,0 +1,29 @@
+// - Since: 05/18/2021
+// - Author: Arkadii Hlushchevskyi
+// - Copyright: © 2021. Arkadii Hlushchevskyi.
+// - Seealso: https://github.com/adya/TSKit.UI/blob/master/LICENSE.md
+import UIKit
+
+public extension UITraitCollection {
+
+    /// Flag that indicates whether given trait collection corresponds to regular layout in both dimensions (iPad layout).
+    ///
+    /// This layout is asscoiated with iPads.
+    var isRegularLayout: Bool {
+        horizontalSizeClass == .regular && verticalSizeClass == .regular
+    }
+
+    /// Flag that indicates whether given trait collection corresponds to regular layout in horizontal dimension.
+    ///
+    /// This layout is asscoiated with iPads and iPhones Max in landscape orientation.
+    var isLandscapeRegularLayout: Bool {
+        horizontalSizeClass == .regular
+    }
+
+    /// Flag that indicates whether given trait collection corresponds to compact layout in vertical dimension.
+    ///
+    /// This layout is asscoiated with standard iPhones in any orientation or iPhones Max in portrait orientation.
+    var isCompactLayout: Bool {
+        verticalSizeClass == .compact
+    }
+}
